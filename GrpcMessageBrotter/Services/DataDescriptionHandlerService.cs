d12635e1a24e84c1093d8f151e11578988eef0af
utsu <mentalsupernovafox@gmail.com>
2023-05-08 13:29:26 +0300

    git fixing message brotter

--- GrpcMessageBrotter/Services/DataDescriptionHandlerService.cs
+++ GrpcMessageBrotter/Services/DataDescriptionHandlerService.cs
@@ -5,7 +5,7 @@ using Microsoft.Data.Sqlite;
 
 namespace GrpcMessageBrotter.Services;
 
-public class DataDescriptionHandlerService : DataDescripionHandler.DataDescripionHandlerBase
+public class DataDescriptionHandlerService : DataDescripionHandler
 
 {
     public override Task<Empty> UploadMessages(MessageUrlRecordStream request, ServerCallContext context)
@@ -19,6 +19,56 @@ public class DataDescriptionHandlerService : DataDescripionHandler.DataDescripio
         {
             
         });
+    }
+
+    public override Task<MessageDataChunk> GetDataChunk(Empty request, ServerCallContext context)
+    {
+        var res = new MessageDataChunk();
+        using (var connection = new SqliteConnection("Data Source=/Users/utsu/RiderProjects/OttersNetwork/GrpcMessageBrotter/dataset_db.db"))
+        {
+            connection.Open();
+
+            // Get the total number of rows in the table
+            using (var countCommand = new SqliteCommand("SELECT COUNT(*) FROM UrlRecord", connection))
+            {
+                int rowCount = Convert.ToInt32(countCommand.ExecuteScalar());
+
+                // Create a parameterized select command
+                using (var selectCommand = new SqliteCommand("SELECT * FROM UrlRecord", connection))
+                {
+                    // If there are more than 100 rows, limit the results to 100
+                    if (rowCount > 100)
+                    {
+                        selectCommand.CommandText += " LIMIT 100";
+                    }
+
+                    // Execute the command and read the results
+                    using (var reader = selectCommand.ExecuteReader())
+                    {
+                        // Check if there are any records returned
+                        if (reader.HasRows)
+                        {
+                            // Loop through the records and do something with them
+                            while (reader.Read())
+                            {
+                                var record = new MessageToDownloadRecord();
+                                record.RecordId = reader.GetInt32(0);
+                                record.Url = reader.GetString(1);
+                                res.Records.Add(record);
+
+
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("No records found.");
+                        }
+                    }
+                }
+            }
+        } 
+        return Task.FromResult(res
+       );
     }
 
     public override Task<Empty> UploadImage(MessageUrlRecordImage request, ServerCallContext context)
